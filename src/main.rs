a13e6b7216736bcaaaa6cb1306887dd7e9e3c63a
Bartosz Krawczyk <129431960+SkuliX01@users.noreply.github.com>
2024-07-04 09:36:15 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,16 @@ async fn main() -> Result<(), sqlx::Error> {
         .connect("")
         .await?;
     
-    let mut create_table_query: QueryBuilder<Postgres> = QueryBuilder::new(
+    let mut create_table_students_query: QueryBuilder<Postgres> = QueryBuilder::new(
         "CREATE TABLE IF NOT EXISTS students(id SERIAL PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, student_age INT NOT NULL)"
     );
 
-    create_table_query.build().execute(&pool).await?;
+    let mut create_table_teachers_query: QueryBuilder<Postgres> = QueryBuilder::new(
+        "CREATE TABLE IF NOT EXISTS teachers(id SERIAL PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, teacher_age INT NOT NULL)"
+    );
+
+    create_table_students_query.build().execute(&pool).await?;
+    create_table_teachers_query.build().execute(&pool).await?;
 
     println!("Welcome to School Management System!");
 
@@ -25,18 +30,23 @@ async fn main() -> Result<(), sqlx::Error> {
         println!("1. Add new student");
         println!("2. Remove student");
         println!("3. See all students");
-        println!("4. Exit");
+        println!("4. Add Teacher");
+        println!("5. Remove Teacher");
+        println!("6. Add Teacher Absence");
+        println!("7. Exit");
 
         let option = utils::get_input("Enter a Choice: ");
 
-        if option.trim() == "5"  {
+        if option.trim() == "7"  {
             println!("Exiting...");
             break;
         }
 
         match option.trim() {
             "1" => utils::register_student(&pool).await?,
+            "2" => utils::remove_student_by_id(&pool).await?,
             "3" => utils::get_all_students(&pool).await?,
+            "4" => utils::register_teacher(&pool).await?,
             _ => println!("Invalid option. Please choose a valid operation."),
         }
     }
